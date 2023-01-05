5d1119ee0ed9a3269c25783d81befece2285147a
Jakboby <88844790+Jakboby@users.noreply.github.com>
2023-01-05 12:10:02 -0800

    Update csas8.cpp
    Function for figure eight. Incredibly unformatted and poorly written.

--- csas8.cpp
+++ csas8.cpp
@@ -6,23 +6,21 @@ using namespace std;
 
 //Required Functions:
 
-void topLine(ofstream& oFile, int size, char c, string symbolName); //Anthony
+void makeSlash(ofstream& oFile, int size, char c);
 
-void makeSlash(ofstream& oFile, int size, char c); //Sebastian
+void makeEight(ofstream& oFile, int size, char c);
 
-void makeEight(ofstream& oFile, int size, char c); //Charles
+void makeTriangle(ofstream& oFile, int size, char c);
 
-void makeTriangle(ofstream& oFile, int size, char c); //Anthony
+bool readRecord(ifstream& iFile, string& typeString, int& lSize, char& c);
 
-bool readRecord(ifstream& iFile, string& typeString, int& lSize, char& c); //Yu Peng
+void openInputFile(ifstream& iFile, string prompt);
 
-void openInputFile(ifstream& iFile, string prompt); //Miracle
+bool openOutputFile(ofstream& oFile, string fname);
 
-bool openOutputFile(ofstream& oFile, string fname); //Yu Peng
+string createOutputFilename(string typeString, int labelSize);
 
-string createOutputFilename(string typeString, int labelSize); //Miracle
-
-void outputChar(ofstream& oFile, int count, char outChar); //Anthony
+void outputChar(ofstream& oFile, int count, char outChar);
 
 
 int main() {
@@ -61,3 +59,39 @@ int main() {
 	return 0;
 }
 
+void makeEight(ofstream& oFile, int size, char c);
+{
+    int middle = 0;
+    int edgeWidth = 0;
+    int rowCount = 0;
+    if (size < 10)
+    {
+        size = 10;
+    }
+    edgeWidth = size / 10;
+    if (edgeWidth < 2)
+    {
+        edgeWidth = 2;
+    }
+    rowCount = edgeWidth / 2;
+    if (size % 2 == 0)
+    {
+        int middle = size / 2;
+    }
+    else
+    {
+        middle = (size / 2) + 1;
+    }
+    for (int currentRow = 1; currentRow <= size; currentRow++)
+    {
+        cout << (currentRow + 1) + "  ";
+        if ((size - currentRow == size - 1) || (size - currentRow == middle) || (size - currentRow == 0))
+        {
+             for (int i = 0; i < edgeWidth; i++)
+             {
+                   cout << " ";
+             }
+        }
+        
+    }
+}
