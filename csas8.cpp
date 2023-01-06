f84dabcf9797d21d14bc5ed23b0e94033db82771
Anthony Saldana Valle <55422282+ansavanix@users.noreply.github.com>
2023-01-05 18:58:57 -0800

    Bring back Charles's makeEight function.

--- csas8.cpp
+++ csas8.cpp
@@ -80,4 +80,39 @@ bool openOutputFile(ofstream& oFile, string fname)
 	return false;
 }
 
+void makeEight(ofstream& oFile, int size, char c);
+{
+    int middle = 0;
+    int edgeWidth = 0;
+    int rowCount = 0;
+    if (size < 10)
+    {
+        size = 10;
+    }
+    edgeWidth = size / 10;
+    if (edgeWidth < 2)
+    {
+        edgeWidth = 2;
+    }
+    rowCount = edgeWidth / 2;
+    if (size % 2 == 0)
+    {
+        int middle = size / 2;
+    }
+    else
+    {
+        middle = (size / 2) + 1;
+    }
+    for (int currentRow = 1; currentRow <= size; currentRow++)
+    {
+        cout << (currentRow + 1) + "  ";
+        if ((size - currentRow == size - 1) || (size - currentRow == middle) || (size - currentRow == 0))
+        {
+             for (int i = 0; i < edgeWidth; i++)
+             {
+                   cout << " ";
+             }
+        }
 
+    }
+}
