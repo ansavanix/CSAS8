2506a411ff1ec52aaea77f65d638389f785db714
Jakboby <88844790+Jakboby@users.noreply.github.com>
2023-01-09 11:18:58 -0800

    Update csas8.cpp

--- csas8.cpp
+++ csas8.cpp
@@ -85,48 +85,6 @@ bool openOutputFile(ofstream& oFile, string fname)
 	return false;
 }
 
-void makeEight(ofstream& oFile, int size, char c)
-{
-	topLine(oFile, size, c, "Figure Eight");
-	xAxis(oFile, size);
-	oFile << endl;
-    int middle = 0;
-    int edgeWidth = 0;
-    int rowCount = 0;
-    if (size < 10)
-    {
-        size = 10;
-    }
-    edgeWidth = size / 10;
-    if (edgeWidth < 2)
-    {
-        edgeWidth = 2;
-    }
-    rowCount = edgeWidth / 2;
-    if (size % 2 == 0)
-    {
-        middle = size / 2;
-    }
-    else
-    {
-        middle = (size / 2) + 1;
-    }
-    for (int currentRow = 1; currentRow <= size; currentRow++)
-    {
-        oFile << (currentRow + 1) + "  ";
-        if ((size - currentRow == size - 1) || (size - currentRow == middle) || (size - currentRow == 0))
-        {
-             for (int i = 0; i < edgeWidth; i++)
-             {
-                   oFile << " ";
-             }
-        }
-
-    }
-    oFile << endl;
-    xAxis(oFile, size);
-}
-
 void topLine(ofstream& oFile, int size, char c, string symbolName) {
 	oFile << "Symbol: " << symbolName << " Size: " << size << " Character: '" << c << "'" << endl;
 }
@@ -192,3 +150,63 @@ grid++;
   }
   }
 //end slash
+
+void makeEight(ofstream& oFile, int size, char c)
+{
+	topLine(oFile, size, c, "Figure Eight");
+	xAxis(oFile, size);
+	oFile << endl;
+    int middle = 0;
+    int edgeWidth = 0;
+    int rowCount = 0;
+    if (size < 10)
+    {
+        size = 10;
+    }
+    edgeWidth = size / 10;
+    if (edgeWidth < 2)
+    {
+        edgeWidth = 2;
+    }
+    rowCount = edgeWidth / 2;
+    if (size % 2 == 0)
+    {
+        middle = size / 2;
+    }
+    else
+    {
+        middle = (size / 2) + 1;
+    }
+    for (int currentRow = 1; currentRow <= size; currentRow++)
+    {
+        oFile << (currentRow + 1) + "  ";
+        if ((size - currentRow == size - 1) || (size - currentRow == middle) || (size - currentRow == 0))
+        {
+             for (int i = 0; i < edgeWidth; i++)
+             {
+                   oFile << " ";
+             }
+             outputChar(oFile, length, c);
+        }
+	else
+	{
+		outputChar(oFile, edgeWidth, c);
+		outputChar(oFile, length, " ");
+		outputChar(oFile, edgeWidth, c);
+	}
+	if (currentRow == (size - 1))
+	{
+		if (size % 2 == 0)
+		{
+		outputChar(oFile, edgeWidth, c);
+		outputChar(oFile, length, " ");
+		outputChar(oFile, edgeWidth, c);
+		}
+	}
+	oFile << endl;
+        }
+
+    }
+    oFile << endl;
+    xAxis(oFile, size);
+}
